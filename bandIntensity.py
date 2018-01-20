96f639d641847fcf4852f0b6b964ecc4024cb7a2
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2018-01-20 15:57:43 -0500

    adding chnages

--- bandIntensity.py
+++ bandIntensity.py
@@ -1,33 +1,63 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 18 11:35:04 2018
 
-@author: paulk
-"""
 class bandIntensity:
     """this class take a txt file input and calculates the values
     band intensity and outputs the values"""
+    global nArray
     global mean
-    bandavg = [46.672, 43.102, 47.29, 109.98, 29.005, 75.975, 69.516, 110.245]
-    sum = 0
+    global array
     
-    def getIntensity(array):        
+    array = []
+    nArray = []
+        
+    
+    def readText(self, text): 
+        with open(text, 'r') as file:
+            data = file.readlines()
+            for line in data:
+                array.extend([float(i) for i in line.split()])            
+    
+    def getIntensity(self):        
         #create a new array, add zero elements until
         #it reaches the size of the input
-        nArray = [0]
-        for i in len(array-1):
-            nArray.append(0)
-            
-        for i in array:
-            nArray[i] = 255 - array[i]
-            print(nArray[i])
-    
-    def getMeanIntensity(array):
-        sum = 0
-        for i in array:
-            sum += array[i]
-            
-        mean = sum/len(array)
+        n = len(array)
+        #print(n)
+        for i in range(n):
+            data = 255 - array[i]
+            nArray.append(data)
         
-    def outputValue():
+    def addBackground(self):
+        ##changes the nArray by the value of the background intensity
+        print('what is the background intensity?: ')
+        background = float(input())
+        n = len(array)
+        for i in range(n):
+            nArray[i] = nArray[i] - background
+    
+    def getMeanIntensity(self):
+        sum = 0
+        n = len(array)
+        print(n)
+        for i in range(n):
+            sum += nArray[i] 
+            
+        global mean 
+        mean = sum/n
+        return mean
+    
+    def outputValue(self):
         print("Average intensity: {}".format(mean))
+
+
+class main:
+    """Calls the intensity function"""
+    intensity = bandIntensity()
+    data = 'C:/Users/paulk/Documents/DNAmean.txt'
+    intensity.readText(data)
+    intensity.getIntensity()
+    intensity.addBackground()
+    intensity.getMeanIntensity()
+    intensity.outputValue()
+    
+    
+    
+    
