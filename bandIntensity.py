210ce6d9c6e56b5fef5b29304d1ac4cf26405d9c
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2018-01-26 12:38:57 -0500

    commiting some gui changes

--- bandIntensity.py
+++ bandIntensity.py
@@ -1,5 +1,5 @@
 import tkinter
-
+from pathlib import Path
 
 class bandIntensity:
     """this class take a txt file input and calculates the values
@@ -9,9 +9,14 @@ class bandIntensity:
     global array
     
     array = []
-    nArray = []    
+    nArray = []  
     
-    
+    def tryText(self, text):
+        try_file = Path(text)
+        if try_file.is_file():
+            return True
+        else:
+            return False
         
     def readText(self, text): 
         with open(text, 'r') as file:
@@ -42,11 +47,14 @@ class bandIntensity:
         
     def addBackground(self):
         ##changes the nArray by the value of the background intensity
+        ##setting the gui object
         print('what is the background intensity?: ')
         background = float(input())
         n = len(array)
         for i in range(n):
             nArray[i] = nArray[i] - background
+        return nArray
+        
     
     def getMeanIntensity(self):
         sum = 0
@@ -83,21 +91,25 @@ class getDocument:
         return new_str
 
 class gui(tkinter.Tk):
+    
     def __init__(self, parent):
         tkinter.Tk.__init__(self, parent)
         self.parent = parent
         
     def initializeGUI(self):
         ##this method contains all gui elements
+        global pathVar
         self.grid()
         
         self.entryVariable = tkinter.StringVar()
         self.entry = tkinter.Entry(self, textvariable = self.entryVariable)
         #for some reason i must include EW
         #this is simply positioning
-        self.entry.grid(column = 0, row = 0, sticky = 'EW')
+        #note column span is a measure of how many active columns the widget spans
+        #ie if we had thre columns active and i chose to span 2 columns, it would leave one blank
+        self.entry.grid(column = 0, row = 0, columnspan = 1,sticky = 'EW')
         self.entry.bind("<Return>", self.OnPressEnter)
-        #self.entryVariable.set(u"enter text here: ")
+        #self.entryVariable.set(u"enter file destination here: ")
         
         
         ##adds a button object to the window
@@ -107,10 +119,18 @@ class gui(tkinter.Tk):
         
         #binding to the label widget
         self.labelVariable = tkinter.StringVar()
+        self.arrayLabelVar = tkinter.StringVar()
+        self.meanLabelVar = tkinter.StringVar()
         #ensures the label is set
         label = tkinter.Label(self, textvariable = self.labelVariable, anchor="w", fg = "white", bg = "blue")
         label.grid(column = 0, row = 1, columnspan = 2, sticky="EW")
-        self.labelVariable.set(u"hello !")
+        ArrayLabel = tkinter.Label(self, textvariable = self.arrayLabelVar, anchor='w', fg = 'white', bg = 'blue')
+        ArrayLabel.grid(column = 0, row = 2, columnspan = 2, sticky = "EW")
+        MeanLabel = tkinter.Label(self, textvariable = self.meanLabelVar, anchor ='w', fg = 'white', bg = 'blue')
+        MeanLabel.grid(column = 0, row = 3, columnspan = 2, sticky = 'EW')
+        self.labelVariable.set(u"Enter the file destination above")
+        self.arrayLabelVar.set(u"mean array: empty")
+        self.meanLabelVar.set(u"currently no sample mean")
         
         self.grid_columnconfigure(0, weight = 1)        
         ##resizes horizontally but not vertically
@@ -119,46 +139,56 @@ class gui(tkinter.Tk):
         self.update()
         self.geometry(self.geometry())
         self.entry.focus_set()
-        self.entry.selection_range(0, tkinter.END)
+        self.entry.selection_range(0, tkinter.END)     
         
-        
-        
-    def OnButtonClick(self): 
-        #self.entryVariable.set("")
-        print(self.entryVariable)
+    def OnButtonClick(self):
+        pathVar = str(self.entryVariable.get())
+        print(self.entryVariable.get())
         self.labelVariable.set(self.entryVariable.get())
         self.entry.focus_set()
-        self.entry.selection_range(0, tkinter.END)        
+        self.entry.selection_range(0, tkinter.END)    
         
     def OnPressEnter(self, event):
-        #self.entryVariable.set("")
+        pathVar = 'null'
+        gDoc = getDocument()
+        bInt = bandIntensity()
+        
+        pathVar = self.entryVariable.get()
         print(self.entryVariable.get())
         self.labelVariable.set(self.entryVariable.get())
         self.entry.focus_set()
         self.entry.selection_range(0, tkinter.END)
-
+        
+        if(pathVar != 'null' or ''):
+            parse = gDoc.process(pathVar)
+            if(bInt.tryText(parse)):
+                bInt.readText(parse)
+                self.proccessData()
+            else:
+                print("this isnt a valid file location")
+                self.labelVariable.set("this is not a valid path")
+    
+    def proccessData(self):
+        bI = bandIntensity()
+        bI.getIntensity()
+        mArray = bI.addBackground()
+        total_mean = bI.getMeanIntensity()
+        self.outsideInit(mArray, total_mean)
+        bI.outputValue()
+        
+    def outsideInit(self,MeanVar, MeanTot):
+        #i didnt end up changing functionality to change the path display because this wouldnt be
+        #acessed if i could not
+        self.arrayLabelVar.set(MeanVar)
+        self.meanLabelVar.set(MeanTot)      
+        
 class main:
+    global pVar
     '''Calls the gui class'''
     guiMain = gui(None)
-    guiMain.title("Band Intensity Widget")
-    guiMain.initializeGUI()
-    ##allows each portion of the gui to loop indefinately
+    guiMain.initializeGUI()        
     guiMain.mainloop()
     
-    """Calls the intensity and getdocument function"""
-    document = getDocument()
-    location = document.getFile()
-    doc = document.process(location)
-    
-    
-    print("this is the doc: ", doc)
-    #calls the intensity class
-    inten = bandIntensity()
-    inten.readText(doc)
-    inten.getIntensity()
-    inten.addBackground()
-    inten.getMeanIntensity()
-    inten.outputValue()
     
     
     
