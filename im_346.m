f0f129d3d1cc60b694a962a55c5868e5b5ff611c
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2018-04-03 11:25:48 -0400

    committing changes

--- im_346.m
+++ im_346.m
@@ -53,3 +53,20 @@ corr2 = im_decomp('comp_3.jpg', 2);
 corr3 = im_decomp('comp_4.jpg', 3);
 corr4 = im_decomp('comp_5.jpg', 4);
 corr5 = im_decomp('comp_6.jpg', 5);
+
+%%      difference for hek images
+
+diff_hek1 = im_diff('hek_1.jpg', 1);
+diff_hek2 = im_diff('hek_2.jpg', 2);
+diff_hek3 = im_diff('hek_3.jpg', 3);
+diff_hek4 = im_diff('hek_4.jpg', 4);
+diff_hek5 = im_diff('hek_5.jpg', 5);
+diff_hek6 = im_diff('hek_composite1.jpg', 6);
+
+%%      cross correlation for hek images 
+corr1 = im_decomp('hek_1.jpg', 1);
+corr2 = im_decomp('hek_2.jpg', 2);
+corr3 = im_decomp('hek_3.jpg', 3);
+corr4 = im_decomp('hek_4.jpg', 4);
+corr5 = im_decomp('hek_5.jpg', 5);
+corr6 = im_decomp('hek_composite1.jpg', 6);
